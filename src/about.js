0b8746726bd2b9591109d6f7ff14f70bff14eddf
mypaxpax <thomasgade@gmail.com>
2023-01-11 17:45:39 +0100

    map imported

--- src/about.js
+++ src/about.js
@@ -1,14 +1,20 @@
+import image from "./resources/images/map.jpg";
+
 function about() {
   const aboutUs = document.createElement("div");
   aboutUs.classList.add("aboutUs");
 
   const phone = document.createElement("p");
-  phone.textContent = "42 42 42 42";
+  phone.textContent = "Call us 📞: 42 42 42 42";
 
-  const location = document.createElement("p");
-  location.textContent = "tempstreet 44";
+  const address = document.createElement("p");
+  address.textContent = "Find us here:🏠";
+
+  const location = document.createElement("img");
+  location.src = image;
 
   aboutUs.appendChild(phone);
+  aboutUs.appendChild(address);
   aboutUs.appendChild(location);
 
   return aboutUs;
